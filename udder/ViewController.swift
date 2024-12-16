e03671fe93dcef984d591ce14e95321184a50b67
wendy <w2699033@gmail.com>
2024-12-16 16:06:12 +0800

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by Wendy Hsiao on 2024/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ udderTests/udderTests.swift
@@ -0,0 +1,17 @@
+//
+//  udderTests.swift
+//  udderTests
+//
+//  Created by Wendy Hsiao on 2024/12/16.
+//
+
+import Testing
+@testable import udder
+
+struct udderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
